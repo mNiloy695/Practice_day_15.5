478c52b5175987a5b637dd7d4360d96ae0bf95ed
mNiloy695 <mniloy695@gmail.com>
2024-06-03 11:35:41 +0600

    first_comment

---
+++ musician/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from Album.models import Album_Model
+class Musician_Model(models.Model):
+    name=models.CharField(max_length=100)
+    email=models.EmailField(max_length=100)
+    phone=models.CharField(max_length=11)
+    album=models.ForeignKey(Album_Model,on_delete=models.SET_NULL,null=True)
+    Instrument_type=models.CharField(max_length=200)
+    def __str__(self) -> str:
+        return self.name
